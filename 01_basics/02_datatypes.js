67af21b50f663710980ae3a4ce11c41a47ea4aac
Code Plate <psengupta1973@gmail.com>
2025-04-20 19:43:19 +0000

    datatypes done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all JS code as newer version
+
+let name = "partha";
+let age = 50; // number -> ranges upto 2 to the power of 53
+let isLoggedIn = false;
+let state = null;
+let passwrd;
+
+// bigint 
+// string -> "" / ''
+// boolean -> true / false
+// null -> standalone value
+// undefined 
+// symbol -> denote uniqueness
+// object -> 
+
+console.table([typeof state, typeof age, typeof passwrd]);
+
