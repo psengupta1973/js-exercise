6f36f51c3cb1e180d0fe22d2e51d1400e4dd800a
Code Plate <psengupta1973@gmail.com>
2025-04-30 19:20:33 +0000

    operations added

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,21 @@
+let someNumber = 11
+let negVal = -someNumber
+console.log(negVal)
+
+console.log(2+3)
+console.log(3-2)
+
+console.log(3*2)
+console.log(3/2)
+console.log(3%2)
+console.log(3**2)
+
+let str1 = "hello"
+let str2 = "partha"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log(1 + 2 + "2")
+
